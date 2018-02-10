d3a6ffa6659c5060cc6605671d05df267cbf3467
panda620812 <panda620812@gamil.com>
2018-02-10 16:05:28 +0800

    panda
    hhh

--- main.c
+++ main.c
@@ -1,5 +1,3 @@
-
-
 /***************************************************************************************************
 *                    (c) Copyright 1992-2009 Embedded Products Research Center
 *                                       All Rights Reserved
@@ -8,7 +6,6 @@
 *\Description   位操作模块，提供丰富的位操作函数
 *               创建文件。
 ***************************************************************************************************/
-
 # include "stdio.h"
 # define u8 	unsigned char 
 # define u16 	unsigned short
@@ -16,13 +13,13 @@
 # define u64 	unsigned long
 # define FALSE 	0
 # define TRUE 	1 
-
 enum
 {
 	FALSE;
 	TRUE;
 }bool;
 
+bool BOOL;
 
 unsigned char AreaInit(void)
 {
@@ -198,7 +195,7 @@ u64 BitValueNot(u64 value, u8 pos)
 }
 
 /***************************************************************************************************
-*\Function      BitValueGetSetNum
+*\Function      BitValueGetSetNum   二分法
 *\Description   返回有效位的数目。
 *\Parameter     value     数据地址
 *\Return        u8
@@ -382,6 +379,16 @@ bool ByteU32ArrayBitGet(u32* data, u16 index)
         return FALSE;
     }
 }
+int findNumberOfTrailingZeros(unsigned int v)
+{
+    static const int MultiplyDeBruijnBitPosition[32] =
+    {
+        0, 1, 28, 2, 29, 14, 24, 3, 30, 22, 20, 15, 25, 17, 4, 8,
+        31, 27, 13, 23, 21, 19, 16, 7, 26, 12, 18, 6, 11, 5, 10, 9
+    };
+    return MultiplyDeBruijnBitPosition[((uint32_t)((v & -v) * 0x077CB531U)) >> 27];
+}
+
 
 static const u8 LSBTable[32] =
 {
